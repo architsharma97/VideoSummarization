94cccbb07201ef583c1f8544dbff39ae95c35b27
architsharma97 <archit_sharma97@yahoo.com>
2016-10-19 12:45:24 +0530

    Added scripts to download data

---
+++ Data/get_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://data.vision.ee.ethz.ch/gyglim/SumMe/SumMe.zip
+unzip SumMe.zip
+rm -rf SumMe.zip
---
+++ run_this.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#script sets up the directory with necessary stuff
+
+cd ./Data
+chmod +x ./get_data.sh
+./get_data.sh
